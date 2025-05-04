e8610681deae7dcfd035bd150aa1d38b49993c8e
DGBIZT <georgiy.gorlov@gmail.com>
2025-05-04 15:46:26 +0300

    Добавление в гитигнор utils.py и работа над ошибками

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,3 +1,6 @@
+from src.category import Category
+from src.products import Product
+
 def test_category(task):
     assert task.name == "Смартфоны"
     assert (
@@ -7,3 +10,19 @@ def test_category(task):
     assert task.products == ["product1", "product2", "product3"]
     assert task.product_count == 3
     assert task.category_count == 1
+
+def test_category_counters():
+    # запоминаем сколько есть на начало теста
+    current_products_counter = Category.product_count
+    current_category_counter = Category.category_count
+
+    # сейчас меняем счётчики создав пару продуктов и категорию
+    products = [
+        Product('a', 'b', 1, 2), # просто номинальные продукты
+        Product('c', 'd', 3, 4),
+    ]
+    category = Category('a', 'b', products) # это действие поменяло счётчики
+
+    # проверяем что изменилось ровно на столько, на сколько мы планировали
+    assert Category.product_count == current_products_counter + 2
+    assert Category.category_count  == current_category_counter + 1
